263f64c4104232e92a335055fafabf9607db2b4c
Oussama Negraz <oussamaelnegraz@gmail.com>
2021-09-18 12:59:30 +0100

    Third commit

--- script.js
+++ script.js
@@ -6,43 +6,46 @@ var label = document.querySelectorAll("label");
 var channelcilck = true;
 
 liveChannels.addEventListener("click", () => {
-    if (groups.classList.contains("visibility")) {
-        channelcilck = false;
-        liveChannels.classList.remove("hover");
-        // Hide Text
-        groupsContains.forEach((ele) => {            
-            ele.style.opacity ="0";
-        })
-        setTimeout(() => {
-            // Close Groups
-            groups.classList.remove("visibility");
-            // open nav
-            setTimeout(() => {            
-                nav.style.width = "200px";
-                label.forEach((ele) => {  
-                    setTimeout(() => {
-                        ele.style.display = "block";
-                    }, 1000);
-                })
-            }, 1000);
-            channelcilck = true;
-        }, 2000);
-    }
-    else {
-        channelcilck = false;
-        liveChannels.classList.add("hover");
-        label.forEach((ele) => {            
-            ele.style.display = "none";
-        })
-        nav.style.width = "50px";
-        setTimeout(() => {
-            groups.classList.add("visibility");
+    console.log(channelcilck);
+    if (channelcilck == true) {
+        if (groups.classList.contains("visibility")) {
+            channelcilck = false;
+            liveChannels.classList.remove("hover");
+            // Hide Text
+            groupsContains.forEach((ele) => {            
+                ele.style.opacity ="0";
+            })
             setTimeout(() => {
-                groupsContains.forEach((ele) => {
-                    ele.style.opacity = "1";
-                });
-            }, 1000);
-            channelcilck = true;
-        }, 2000);
+                // Close Groups
+                groups.classList.remove("visibility");
+                // open nav
+                setTimeout(() => {            
+                    nav.style.width = "200px";
+                    label.forEach((ele) => {  
+                        setTimeout(() => {
+                            ele.style.display = "block";
+                            channelcilck = true;
+                        }, 1000);
+                    })
+                }, 2000);
+            }, 2000);
+        }
+        else {
+            channelcilck = false;
+            liveChannels.classList.add("hover");
+            label.forEach((ele) => {            
+                ele.style.display = "none";
+            })
+            nav.style.width = "50px";
+            setTimeout(() => {
+                groups.classList.add("visibility");
+                setTimeout(() => {
+                    groupsContains.forEach((ele) => {
+                        ele.style.opacity = "1";
+                    });
+                    channelcilck = true;
+                }, 1000);
+            }, 2000);
+        }
     }
 })
